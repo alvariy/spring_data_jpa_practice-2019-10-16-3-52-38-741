d15459cb0101652dd1ef8cc4f0fb717d2bf3e525
IYAN OLIVER ALVAREZ (PLAN-IRIS-ISD-OOCLL/MNL) <ALVARIY@OOCL.COM>
2019-10-17 11:08:13 +0800

    Redesign some schema for prosecutor

--- src/main/resources/db.migration/V1__Create_Prosecutor_Table.sql
+++ src/main/resources/db.migration/V1__Create_Prosecutor_Table.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS 'prosecutor'
 
 CREATE TABLE 'prosecutor'(
-      'id' BINARY(16) NOT NULL,
+      'id' BINARY(16) NOT NULL DEFAULT random_uuid(),
       'name' varchar(255) DEFAULT NULL,
       PRIMARY KEY ('id')
   );
